c117e61b01bff6e698f7c17b1713af37cefd7a64
e.belov <belov.e.v@effective-group.ru>
2020-11-25 22:03:57 +0300

    - add files in .gitignore - update composer.json - update index.php

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 
 use Dotenv\Dotenv;
-use Rosatom\Auth\Presentation\AuthRouter;
 use Rosatom\Common\DBConnect;
-use Rosatom\FinReport\FinReportRoute;
-use Rosatom\RateCurrencies\Presentation\RateOfCurrencyRouter;
-use Rosatom\Reports\Presentation\ReportsRouter;
-use Rosatom\User\Presentation\UserRouter;
 use Slim\App;
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/src/vendor/autoload.php';
@@ -19,26 +14,6 @@ $config = ['settings' => ['displayErrorDetails' => true]];
 $app = new App($config);
 
 DBConnect::init();
-
-$app->group('/reports', function () {
-    return new ReportsRouter($this);
-});
-
-$app->group('/auth', function () {
-    return new AuthRouter($this);
-});
-
-$app->group('/user', function () {
-    return new UserRouter($this);
-});
-
-$app->group('/rateOfCurrency', function () {
-    return new RateOfCurrencyRouter($this);
-});
-
-$app->group('/finreport', function () {
-    return new FinReportRoute($this);
-});
 
 
 $app->run();
